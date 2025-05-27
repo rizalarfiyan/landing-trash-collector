4e078c52c799645485414a1eb57384b6705ffabf
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-05-27 08:12:17 +0700

    feat: update with google analytics
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,37 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://trash-collector.rizalarfiyan.com',
+  output: 'static',
+  integrations: [
+    partytown({
+      config: {
+        forward: ['dataLayer.push'],
+        debug: false,
+      }
+    }),
+  ],
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [
+      tailwindcss(),
+    ],
+    esbuild: {
+      legalComments: 'none',
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: '[hash].js'
+        }
+      },
+    },
+  },
+  build: {
+    assets: 'public',
+  },
+  scopedStyleStrategy: 'where'
 });
