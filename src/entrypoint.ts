2a7736c6e379cbd3feb43ab47c24ad2ca4358a8f
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-06-30 22:14:54 +0700

    feat: add section partners
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -3,10 +3,12 @@ import intersect from '@alpinejs/intersect'
 import scrollTo from '@lib/scroll-to'
 import counter from '@lib/counter'
 import slider from '@lib/slider'
+import logoWall from '@lib/logo-wall'
 
 export default (Alpine: Alpine) => {
     Alpine.plugin(intersect)
     Alpine.magic('scrollTo', scrollTo)
     Alpine.data('counter', counter)
     Alpine.data('slider', slider)
+    Alpine.data('logoWall', logoWall)
 }
