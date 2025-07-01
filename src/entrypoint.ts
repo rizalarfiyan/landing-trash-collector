4cfa1cc5e63eb8fe60c824cb46ac9e77d6ebd588
Rizal Arfiyan <rizal.arfiyan.23@gmail.com>
2025-07-01 07:25:41 +0700

    feat: update all sections
    Signed-off-by: Rizal Arfiyan <rizal.arfiyan.23@gmail.com>

--- src/entrypoint.ts
+++ src/entrypoint.ts
@@ -1,14 +1,18 @@
 import type { Alpine } from 'alpinejs'
 import intersect from '@alpinejs/intersect'
+import collapse from '@alpinejs/collapse'
 import scrollTo from '@lib/scroll-to'
 import counter from '@lib/counter'
 import slider from '@lib/slider'
 import logoWall from '@lib/logo-wall'
+import accordion from '@lib/accordion'
 
 export default (Alpine: Alpine) => {
     Alpine.plugin(intersect)
+    Alpine.plugin(collapse)
     Alpine.magic('scrollTo', scrollTo)
     Alpine.data('counter', counter)
     Alpine.data('slider', slider)
     Alpine.data('logoWall', logoWall)
+    Alpine.data('accordion', accordion)
 }
